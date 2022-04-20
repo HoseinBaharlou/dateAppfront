34c0dcc7b3544994585b21a2e9ef37b15b476003
HoseinBaharlou <hossinbahar98@gmail.com>
2022-04-20 09:28:30 +0430

    create services page and header component

---
+++ pages/services.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="mt-75">
+    <!-- start header -->
+    <Header />
+    <!-- end header -->
+
+    <!-- start service -->
+
+    <!-- end service -->
+  </v-container>
+</template>
+<script>
+import Header from '../components/partials/services/header.vue'
+export default {
+  setup() {
+    
+  },
+  components:{Header}
+}
+</script>
