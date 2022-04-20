1cd45fe2794e2a5dbec51036faa712eb604f1f83
HoseinBaharlou <hossinbahar98@gmail.com>
2022-04-20 08:37:23 +0430

    create global food component

---
+++ components/partials/about/GlobalFood.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-row>
+    <!-- start title and subtitle-->
+    <v-col cols="12" md="6">
+      <p class="green-light text-center text-md-left">Photo Gallery</p>
+      <h1 class="blue-gray-text font-weight-bold font-size-60 text-md-left text-center">We're Global Food Delivery <span class="green-border">Organic Company</span></h1>
+    </v-col>
+    <!-- end title and subtitle -->
+
+    <!-- start timeline and description -->
+    <v-col cols="12">
+      <v-row>
+        <v-col md="3">
+          <!-- start custom timeline -->
+          <div class="d-flex align-center">
+            <v-sheet class="rounded-circle bg-green-darken-to-light" elevation="2" height="40" width="42"></v-sheet>
+            <v-divider class=" ml-5"></v-divider>
+          </div>
+          <!-- end custom timeline -->
+
+          <!-- start description and title -->
+          <div class="mt-2">
+            <h2 class="blue-gray-text">1986</h2>
+            <h5 class="blue-gray-text">Vivamus sagittis dictum eros, at fin</h5>
+            <p class="font-size-17 font-weight-light grey--text mt-2">
+              Nullam viverra nibh sit amet lectus mollis condimentum. Integer ullamcorper velit ac viverra vestibulum
+            </p>
+          </div>
+        </v-col>
+
+        <v-col md="3">
+          <!-- start custom timeline -->
+          <div class="d-flex align-center">
+            <v-img src="icons/Group 11.svg" lazy-src="icons/Group 11.svg" max-width="42" max-height="40" />
+            <v-divider class=" ml-5"></v-divider>
+          </div>
+          <!-- end custom timeline -->
+
+          <!-- start description and title -->
+          <div class="mt-2">
+            <h2 class="blue-gray-text">1986</h2>
+            <h5 class="blue-gray-text">Vivamus sagittis dictum eros, at fin</h5>
+            <p class="font-size-17 font-weight-light grey--text mt-2">
+              Nullam viverra nibh sit amet lectus mollis condimentum. Integer ullamcorper velit ac viverra vestibulum
+            </p>
+          </div>
+        </v-col>
+
+        <v-col md="3">
+          <!-- start custom timeline -->
+          <div class="d-flex align-center">
+            <v-img src="icons/Group 11.svg" lazy-src="icons/Group 11.svg" max-width="42" max-height="40" />
+            <v-divider class=" ml-5"></v-divider>
+          </div>
+          <!-- end custom timeline -->
+
+          <!-- start description and title -->
+          <div class="mt-2">
+            <h2 class="blue-gray-text">1986</h2>
+            <h5 class="blue-gray-text">Vivamus sagittis dictum eros, at fin</h5>
+            <p class="font-size-17 font-weight-light grey--text mt-2">
+              Nullam viverra nibh sit amet lectus mollis condimentum. Integer ullamcorper velit ac viverra vestibulum
+            </p>
+          </div>
+        </v-col>
+
+        <v-col md="3">
+          <!-- start custom timeline -->
+          <div class="d-flex align-center">
+            <v-img src="icons/Group 11.svg" lazy-src="icons/Group 11.svg" max-width="42" max-height="40" />
+            <v-divider class=" ml-5"></v-divider>
+          </div>
+          <!-- end custom timeline -->
+
+          <!-- start description and title -->
+          <div class="mt-2">
+            <h2 class="blue-gray-text">1986</h2>
+            <h5 class="blue-gray-text">Vivamus sagittis dictum eros, at fin</h5>
+            <p class="font-size-17 font-weight-light grey--text mt-2">
+              Nullam viverra nibh sit amet lectus mollis condimentum. Integer ullamcorper velit ac viverra vestibulum
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-col>
+    <!-- end timeline and description -->
+  </v-row>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -11,6 +11,10 @@
     <!-- start photo ghalery -->
     <photo-ghalery class="mt-140" />
     <!-- start photo ghalery -->
+
+    <!-- start global food -->
+    <global-food class="mt-110"/>
+    <!-- end global food -->
   </v-container>
 </template>
 
@@ -18,11 +22,12 @@
 import Header from '../components/partials/about/header.vue'
 import aboutCompany from '../components/partials/about/aboutCompany.vue'
 import PhotoGhalery from '../components/partials/about/photoGhalery.vue'
+import GlobalFood from '../components/partials/about/GlobalFood.vue'
 export default {
   setup() {
     
   },
-  components:{Header,aboutCompany,PhotoGhalery},
+  components:{Header,aboutCompany,PhotoGhalery,GlobalFood},
   layout:'default'
 
 }
