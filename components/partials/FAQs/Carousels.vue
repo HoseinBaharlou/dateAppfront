428636fe38276bc15ddc7dac4d30616f99c17530
HoseinBaharlou <hossinbahar98@gmail.com>
2022-04-30 08:39:13 +0430

    create carousel  component for FAQs page and finish FAQs page

---
+++ components/partials/FAQs/Carousels.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-row class="mt-100">
+    <v-col cols="12">
+      <!-- start group slider -->
+      <VueSlickCarousel v-bind="settings">
+        <div class="px-1">
+          <v-img src="images/dates-1067265_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/palma-1454793_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/dates-1067265_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/palma-1454793_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/dates-1067265_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/dates-1067265_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/palma-1454793_1920 1.svg"/>
+        </div>
+        <div class="px-1">
+          <v-img src="images/dates-1067265_1920 1.svg"/>
+        </div>
+      </VueSlickCarousel>
+      <!-- end group slider -->
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  components:{VueSlickCarousel},
+  data:()=>{
+    return{
+      settings:{
+        "dots": true,
+        "infinite": false,
+        "arrows":false,
+        "speed": 500,
+        "slidesToShow": 5,
+        "slidesToScroll": 5,
+        "initialSlide": 0,
+        "responsive": [
+          {
+            "breakpoint": 1024,
+            "settings": {
+              "slidesToShow": 3,
+              "slidesToScroll": 3,
+              "infinite": false,
+              "dots": true
+            }
+          },
+          {
+            "breakpoint": 600,
+            "settings": {
+              "slidesToShow": 2,
+              "slidesToScroll": 2,
+              "initialSlide": 2
+            }
+          },
+          {
+            "breakpoint": 480,
+            "settings": {
+              "slidesToShow": 1,
+              "slidesToScroll": 1
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
--- pages/FAQs.vue
+++ pages/FAQs.vue
@@ -6,16 +6,21 @@
     <!-- start FAQs -->
     <FAQs class="mt-140"/>
     <!-- end FAQs -->
+
+    <!-- start carousel -->
+    <carousel/>
+    <!-- end carousel -->
   </v-container>
 </template>
 
 <script>
 import Header from '../components/partials/FAQs/header.vue'
 import FAQs from '../components/partials/FAQs/FAQs.vue'
+import carousel from '../components/partials/FAQs/Carousels.vue'
 export default {
   setup() {
     
   },
-  components:{Header,FAQs}
+  components:{Header,FAQs,carousel}
 }
 </script>
